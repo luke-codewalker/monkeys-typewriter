ddee945e9500a58abcac00cebe4fa2149c1928be
Luke Codewalker <arathorn089@arcor.de>
2018-03-10 23:13:34 +0100

    methods for crossover and mutation
    DAN has now two static methods that can crossover two parent DNAs to create a new chil

--- js/app.js
+++
@@ -1,3 +0,0 @@
-const monkeys = new Population('hello', 0.01, 10);
-
-monkeys.population.forEach(monkey => console.log(monkey.phrase))
--- js/dna.js
+++ js/dna.js
@@ -10,4 +10,32 @@ class DNA {
         return this.genes.join('');
     }
 
+    static crossover(parentA, parentB) {
+        const slicePoint = Math.floor(random(parentA.genes.length));
+        const child = new DNA(parentA.genes.length);
+        child.genes = parentA.genes.slice(0, slicePoint).concat(parentB.genes.slice(slicePoint));
+        return child;
+    }
+
+    static mutate(genes, rate) {
+        return genes.map(gene => {
+            if (random() < rate) {
+                return randomChar();
+            } else {
+                return gene;
+            }
+        })
+    }
+
+    fitness(target) {
+        const score = this.genes.reduce((score, geneChar, i) => {
+            if (geneChar === target[i]) {
+                return score++;
+            } else {
+                return score;
+            }
+        }, 0);
+
+        return score / target.length;
+    }
 }
