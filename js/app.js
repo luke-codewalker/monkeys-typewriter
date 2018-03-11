b8178c6b121e23977e349946ed9b555d2d384cb1
Luke Codewalker <arathorn089@arcor.de>
2018-03-11 13:38:22 +0100

    New evolution loop stops old ones
    Introduced a global id to track the most current evolution and stop all old ones whose id doesn't match. This fixes #1.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
-const evolve = (population) => {
-    // evaluate if not done request another evolution
+const evolve = (population, id) => {
+    console.log(id);
+    // if evolution process is not the current one we can quit right away
+    if (id !== evolutionId) return;
+
+    // evaluate the current population
     population.evaluate();
 
     // show the best phrase on the page
@@ -10,31 +14,38 @@ const evolve = (population) => {
         return allPhrases += `<span>${member.phrase}</span>`;
     }, ``);
 
+    // if not done request another evolution
     if (population.bestPhrase !== population.target) {
         select('.avg-fitness').innerText = `${Math.floor(population.averageFitness * 100)} %`;
         select('.generation').innerText = population.generation;
         population.generate();
-        window.requestAnimationFrame(() => evolve(population));
+        window.requestAnimationFrame(() => evolve(population, id));
     } else {
+        // show that we are done
         select('.best-phrase').classList.add('done');
     }
 }
 
+let evolutionId = 0;
+
 select('button').addEventListener('click', () => {
     select('.best-phrase').classList.remove('done');
-
+    // get input values for new population
     const target = select('.target-phrase').value;
     const populationSize = select('.population-size').value;
     const mutationRate = select('.mutation-rate').value;
 
-    if(target.length > 0 &&
-    populationSize >= 100 &&
-    populationSize <= 999 &&
-    mutationRate >= 1 &&
-    mutationRate <= 99) {
+    if (target.length > 0 &&
+        populationSize >= 100 &&
+        populationSize <= 999 &&
+        mutationRate >= 1 &&
+        mutationRate <= 99) {
+        // increase the evolution id to stop the old loop
+        evolutionId++;
+        // use the input to start a new evolution
         const monkeys = new Population(target, populationSize, mutationRate / 100);
         monkeys.init();
-        evolve(monkeys);
-    } 
+        evolve(monkeys, evolutionId);
+    }
 });
 
