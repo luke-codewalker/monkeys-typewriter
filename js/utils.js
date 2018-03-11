40d61df33ee50166c004fcf53113a3034e9d3190
Luke Codewalker <arathorn089@arcor.de>
2018-03-11 16:01:21 +0100

    Fixed #4
    rewrote the function to get random characters so that z doesn't break the page anymore. also included umlauts and more punctuation characters

--- js/utils.js
+++ js/utils.js
@@ -8,7 +8,6 @@ const random = (min = 1, max = 0) => {
 }
 
 const randomChar = () => {
-    let code = Math.floor(random(64, 122));
-    if (code === 64) code = 32;
-    return String.fromCharCode(code);
+    let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ü', 'ö', 'ä', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ä', 'Ö', 'Ü', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'ß', '!', '?', '.', ',', ';', ':', '-', '_', '"', '&', '/', '(', ')', '[', ']', '{', '}', ' ']
+    return chars[Math.floor(random(chars.length))];
 }
